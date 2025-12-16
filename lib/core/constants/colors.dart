3ddb89bb6d419491a2241a8c89bb103d8a8f8045
ifqhllm <148844840+ifqhllm@users.noreply.github.com>
2025-12-16 20:06:09 +0700

    Menambahkan file colors dan homepage

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryRed = Color(0xFFC62828);
+  static const Color secondaryGrey = Color.fromARGB(0, 35, 234, 21);
+  static const Color textDark = Color(0xFF212121);
+}
